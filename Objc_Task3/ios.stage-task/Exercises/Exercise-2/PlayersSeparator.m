22b770732f3bef5a42cb369d20e7ad3e4c2f7f6d
Ivan <waizord@yandex.ru>
2021-05-24 20:03:48 +0300

    Fix: fixed exercise-2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,63 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    NSMutableArray * result = [NSMutableArray new];
+    NSInteger countResult = 0;
+    
+    if (ratingArray.count < 3) {
+        return 0;
+    }
+    
+    for (int i = 0; i < ratingArray.count - 2; i++) { 
+        NSLog(@"Value index i -> %i", i);
+        [result addObject:ratingArray[i]];
+        
+        for (int j = i + 1; j < ratingArray.count - 1; j++) {
+            NSLog(@"Value index j -> %i", j);
+            if ([ratingArray[i] integerValue] < [ratingArray[j] integerValue]) {
+                
+                [result addObject:ratingArray[j]];
+                
+                for (int k = j + 1; k < ratingArray.count; k++) {
+                    NSLog(@"Value index k -> %i", k);
+                    if ([ratingArray[j] integerValue] < [ratingArray[k] integerValue]) {
+                        [result addObject:ratingArray[k]];
+                        if (result.count == 3) {
+                            NSLog(@"Array -> %@", result);
+                            countResult++;
+                            [result removeLastObject];
+                        }
+                    }
+                }
+                [result removeLastObject];
+                
+            } else {
+                if ([ratingArray[i] integerValue] > [ratingArray[j] integerValue]) {
+                    
+                    [result addObject:ratingArray[j]];
+                    
+                    for (int k = j + 1; k < ratingArray.count; k++) {
+                        NSLog(@"Value index k -> %i", k);
+                        if ([ratingArray[j] integerValue] > [ratingArray[k] integerValue]) {
+                            [result addObject:ratingArray[k]];
+                            if (result.count == 3) {
+                                NSLog(@"Array -> %@", result);
+                                countResult++;
+                                [result removeLastObject];
+                                
+                            }
+                        }
+                    }
+                    [result removeLastObject];
+                }
+            }
+        }
+        [result removeLastObject];
+    }
+    NSLog(@"Result count -> %lu", countResult);
+    
+    return countResult;
 }
 
 @end
